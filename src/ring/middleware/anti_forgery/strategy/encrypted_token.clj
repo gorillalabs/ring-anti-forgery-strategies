3cf60bc3b727e1e6fc750f2740d8cef5c55f3a97
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-23 21:25:20 +0100

    change protocol in accordance with https://git.io/vNrge

--- src/ring/middleware/anti_forgery/strategy/encrypted_token.clj
+++ src/ring/middleware/anti_forgery/strategy/encrypted_token.clj
@@ -15,40 +15,42 @@
   (hash/sha256 secret))
 
 (deftype EncryptedTokenSMS [secret expiration-period get-subject-fn]
+
   strategy/StateManagementStrategy
 
-  (token [_ request]
+  (get-token [_ request]
     (delay (jwt/encrypt {:sub (get-subject-fn request)
-                  :jti (crypto.random/base64 512)           ;; the nonce is to secure encryption (i.e. to prevent replay attacks). Used as JWT ID in the JWT (see https://tools.ietf.org/html/rfc7519#section-4.1.7).
-                  :iat (clj-time.coerce/to-epoch (time/now)) ;; Issued at (see https://tools.ietf.org/html/rfc7519#section-4.1.6)
-                  :exp (clj-time.coerce/to-epoch (time/plus (time/now) expiration-period)) ;; Expires (see https://tools.ietf.org/html/rfc7519#section-4.1.4)
-}
-                 secret
-                 crypt-options)))
 
-  (valid-token? [_ request read-token]
-    (when-let [token (read-token request)]
-      (try
-        (let [{:keys [sub]} (jwt/decrypt token
-                                         secret
-                                         crypt-options)]
+                         ;; the nonce is to secure encryption (i.e. to prevent replay attacks).
+                         ;; Used as JWT ID in the JWT (see https://tools.ietf.org/html/rfc7519#section-4.1.7).
+                         :jti (crypto.random/base64 512)
 
-          ;; check subject (must either be empty (now, not at token claim) or equal to the one in the claims)
-          (when-let [subject (get-subject-fn request)]
-            (when-not (crypto/eq? sub subject)
-              (throw (ex-info (str "Subject does not match " sub)
-                              {:type :validation :cause :sub}))))
+                         ;; Issued at (see https://tools.ietf.org/html/rfc7519#section-4.1.6)
+                         :iat (clj-time.coerce/to-epoch (time/now))
 
-          true)
-        (catch ExceptionInfo e
-          (when-not (= (:type (ex-data e)) :validation)
-            (throw e))
-          (when-not (= (:cause (ex-data e)) :exp)
-            (log/warn
-             e
-             "Security warning: Potential CSRF-Attack"
-             (ex-data e)))
-          false))))
+                         ;; Expires (see https://tools.ietf.org/html/rfc7519#section-4.1.4)
+                         :exp (clj-time.coerce/to-epoch (time/plus (time/now) expiration-period))}
+                        secret
+                        crypt-options)))
 
-  (write-token [_ response _ _]
+  (valid-token? [_ request token]
+    (try
+      (let [{:keys [sub]} (jwt/decrypt token
+                                       secret
+                                       crypt-options)]
+        ;; check subject (must either be empty (now, not at token claim) or equal to the one in the claims)
+        (when-let [subject (get-subject-fn request)]
+          (when-not (crypto/eq? sub subject)
+            (throw (ex-info (str "Subject does not match " sub)
+                            {:type :validation :cause :sub}))))
+        true)
+      (catch ExceptionInfo e
+        (when-not (= (:type (ex-data e)) :validation)
+          (throw e))
+        (when-not (= (:cause (ex-data e)) :exp)
+          (log/warn e "Security warning: Potential CSRF-Attack"
+                    (ex-data e)))
+        false)))
+
+  (write-token [_ _ response _]
     response))
