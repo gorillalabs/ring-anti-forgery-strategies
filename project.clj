907edcfbc48748f19dfceab627b825136744c138
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-25 10:53:43 +0100

    change project coordinates of ring-anti-forgery

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gorillalabs/ring-anti-forgery-strategies "1.1.0"
+(defproject gorillalabs/ring-anti-forgery-strategies "0.0.0"
             :description "Ring middleware to prevent CSRF attacks"
             :url "https://github.com/ring-clojure/ring-anti-forgery-strategies"
             :license {:name "The MIT License"
@@ -8,7 +8,7 @@
                          leiningen.v/add-workspace-data]
             :plugins [[com.roomkey/lein-v "6.2.0"]]
             :dependencies [[org.clojure/clojure "1.8.0"]
-                           [gorillalabs/ring-anti-forgery "1.2.0-beta"]
+                           [gorillalabs/ring-anti-forgery "1.3.0-beta"]
                            [org.clojure/tools.logging "0.4.0"]
                            [crypto-random "1.2.0"]
                            [crypto-equality "1.0.0"]
